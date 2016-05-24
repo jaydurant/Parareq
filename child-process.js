04f73a97ceeb8bfb82b31d3630fe5d74ef7576c4
durantjay <durantjay1@gmail.com>
2016-05-24 15:42:05 -0700

    Add child-process script to create process forks

---
+++ child-process.js
@@ -0,0 +1,11 @@
+const child = require('child_process');
+
+const forkCount =  parseInt(process.argv[2]);
+const intervalTime = parseInt(process.argv[3]);
+
+for (var i = 0 ; i < forkCount; i++){
+	process.stdout.write(`Process: ${i} at work`);
+	let childProcess = child.fork('get-request.js',['www.google.com', intervalTime]);
+
+	childProcess.on('close', (code) => {`child process exited with code ${code}`})
+}
