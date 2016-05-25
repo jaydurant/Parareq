eea6df8acad698b96efaf91c88e022d7545df70e
durantjay <durantjay1@gmail.com>
2016-05-25 10:02:39 -0700

    Add targetURI variable to make it easer to change the variable which controls the url which is pinged.

--- child-process.js
+++ child-process.js
@@ -3,10 +3,11 @@ const child = require('child_process');
 const forkCount =  parseInt(process.argv[2]);
 const forkCalls =  parseInt(process.argv[3])
 const intervalTime = parseInt(process.argv[4]);
+const targetURI = 'www.google.com';
 
 for (var i = 0 ; i < forkCount; i++){
 	process.stdout.write(`Process: ${i} at work`);
-	let childProcess = child.fork('get-request.js',['www.google.com',forkCalls ,intervalTime]);
+	let childProcess = child.fork('get-request.js',[targetURI,forkCalls ,intervalTime]);
 
 	childProcess.on('close', (code) => {`child process exited with code ${code}`})
 }
