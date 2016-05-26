a5584da282e24af7aefc67b2a585f41ee4f6661b
durantjay <durantjay1@gmail.com>
2016-05-26 10:49:09 -0700

    Add tr.fuelx.com to get-request.js

--- child-process.js
+++ child-process.js
@@ -3,7 +3,7 @@ const child = require('child_process');
 const forkCount =  parseInt(process.argv[2]);
 const forkCalls =  parseInt(process.argv[3])
 const intervalTime = parseInt(process.argv[4]);
-const targetURI = 'www.google.com';
+const targetURI = 'tr.fuelx.com';
 
 for (var i = 0 ; i < forkCount; i++){
 	process.stdout.write(`Process: ${i} at work`);
